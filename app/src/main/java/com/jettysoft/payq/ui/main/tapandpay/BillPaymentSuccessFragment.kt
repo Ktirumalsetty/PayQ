44657f6a5e1b6aba8aee6b438cb238c40ff772c7
KondalRao <kondalrao.t@ddistechnologies.com>
2022-03-02 19:40:48 +0530

    BillPayment Success UI

--- app/src/main/java/com/jettysoft/payq/ui/main/tapandpay/BillPaymentSuccessFragment.kt
+++ app/src/main/java/com/jettysoft/payq/ui/main/tapandpay/BillPaymentSuccessFragment.kt
@@ -1,13 +1,9 @@
 package com.jettysoft.payq.ui.main.tapandpay
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.jettysoft.payq.R
 import com.jettysoft.payq.databinding.BillPaymentSuccessFragmentBinding
 import com.jettysoft.payq.ui.common.base.BaseFragment
 
@@ -25,6 +21,14 @@ class BillPaymentSuccessFragment : BaseFragment<BillPaymentSuccessFragmentBindin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as WirelessPaymentActivity?)!!.supportActionBar!!.hide()
+
+        binding.apply {
+            btnDone.setOnClickListener(View.OnClickListener {
+                requireActivity().finish()
+            })
+        }
+
     }
 
 
